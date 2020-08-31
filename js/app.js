6fb2e3d7a913743e9afc63e9252274c416b3b19d
Mimi Netto <michnetto249@gmail.com>
2020-08-31 17:21:57 -0400

    monday afternoon commit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,38 @@
-$(() => {
-  $('form').on('submit',event => {
-  event.preventDefault()
-  //grab the text from the input box and store in variable
+    $(() => {
 
-  let userInput = $('input[type="text"]').val()
-  console.log(userInput);
+      let pageLimit = 10;
+      let resultLimit = $('pageLimit').val() || 10
+      console.log(resultLimit);
+      // let villagerID = 2;
+    // ACNH URL
+    let link = `http://acnhapi.com/v1a/villagers/`
 
+    $.ajax({
+      url: link,
+      type: "GET",
+      dataType: "json",
+      data: {
+        // villagerID: {villagerID},
+        $limit: resultLimit,
+      },
 
-  //grab the borough bases on the id of the clicked button store that as well
-  // let species = $(event.target).attr('id')
-  // console.log(species);
-  //set ID bsed on borough clicked
-
-  let link = `http://acnhapi.com/v1a/villagers/`
-
-  $.ajax({
-    url: link,
-    type: "GET",
-
-
-  }).then(villagers => {
-    for (let villager of villagers){
+    }).then(villagers => {
+    for (let villager of villagers) {
       const $villagers = $('.villagers')
-
 
       const $div = $('<div>').appendTo($villagers)
 
       const $h3 = $('<h3>').text(villager.name['name-USen'])
       .appendTo($div)
 
-      const $image =
-      $('<img>')
-      .attr('src',(villager.image_uri))
+      const $h5 = $('<h5>').text(villager.personality)
       .appendTo($div)
 
-
-    }
-
+      const $image =
+        $('<img>')
+        .addClass('photo')
+        .attr('src', (villager.image_uri))
+        .appendTo($div)
+      }
   })
- })
 })
