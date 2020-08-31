e5b1087a76e1e5cb138604e989567ff2f9e1dea7
Mimi Netto <michnetto249@gmail.com>
2020-08-31 18:44:36 -0400

    monday afternoon commit

--- graveyards/gravyard00.js
+++ graveyards/gravyard00.js
@@ -76,3 +76,45 @@ $(()=>{
         );
     })
 })
+
+$(() => {
+
+  let pageLimit = 10;
+  let resultLimit = $('pageLimit').val() || 10
+  console.log(resultLimit);
+  let villagerID = 1;
+
+// ACNH URL
+let link = `http://acnhapi.com/v1/villagers/`
+
+$.ajax({
+  url: link,
+  type: "GET",
+  dataType: "json",
+  data: {
+    villagerID: {villagerID},
+    // personality: personality,
+  },
+
+}).then(villagers => {
+for (let villager of villagers) {
+  const $villagers = $('.villagers')
+
+  const $div = $('<div>')
+  .addClass('profile')
+  .appendTo($villagers)
+
+  const $h3 = $('<h3>').text(villager.name['name-USen'])
+  .appendTo($div)
+
+  // const $h5 = $('<h5>').text(villager.personality)
+  // .appendTo($div)
+
+  const $image =
+    $('<img>')
+    .addClass('photo')
+    .attr('src', (villager.image_uri))
+    .appendTo($div)
+  }
+})
+})
--- js/app.js
+++ js/app.js
@@ -1,32 +1,26 @@
     $(() => {
 
-      let pageLimit = 10;
-      let resultLimit = $('pageLimit').val() || 10
-      console.log(resultLimit);
-      // let villagerID = 2;
     // ACNH URL
-    let link = `http://acnhapi.com/v1a/villagers/`
+    let link = `http://acnhapi.com/v1/villagers/`
 
     $.ajax({
       url: link,
       type: "GET",
       dataType: "json",
-      data: {
-        // villagerID: {villagerID},
-        $limit: resultLimit,
-      },
 
     }).then(villagers => {
     for (let villager of villagers) {
       const $villagers = $('.villagers')
 
-      const $div = $('<div>').appendTo($villagers)
+      const $div = $('<div>')
+      .addClass('profile')
+      .appendTo($villagers)
 
       const $h3 = $('<h3>').text(villager.name['name-USen'])
       .appendTo($div)
 
-      const $h5 = $('<h5>').text(villager.personality)
-      .appendTo($div)
+      // const $h5 = $('<h5>').text(villager.personality)
+      // .appendTo($div)
 
       const $image =
         $('<img>')
