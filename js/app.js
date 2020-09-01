aa4688325695a883b09cbbf790251de07ec77b7f
Mimi Netto <michnetto249@gmail.com>
2020-08-31 20:59:33 -0400

    monday night  commit

--- js/app.js
+++ js/app.js
@@ -1,35 +1,28 @@
-    $(() => {
+$(() => {
+  // ACNH URL
+  let link = `https://acnhapi.com/v1a/villagers/`;
 
-    // ACNH URL
-    let link = `http://acnhapi.com/v1a/villagers/`
+  $.ajax({
+    url: link,
+    type: "GET",
+    dataType: "json",
+  }).then((villagers) => {
+    const sliceArr = villagers.slice(0, 20);
+    console.log(sliceArr);
+    for (let villager of sliceArr) {
+      const $villagers = $(".villagers");
 
-    $.ajax({
-      url: link,
-      type: "GET",
-      dataType: "json",
-      data: {
-        maxResults: 10,
-      },
+      const $div = $("<div>").addClass("profile").appendTo($villagers);
 
-    }).then(villagers => {
-    for (let villager of villagers) {
-      const $villagers = $('.villagers')
-
-      const $div = $('<div>')
-      .addClass('profile')
-      .appendTo($villagers)
-
-      const $h3 = $('<h3>').text(villager.name['name-USen'])
-      .appendTo($div)
+      const $h3 = $("<h3>").text(villager.name["name-USen"]).appendTo($div);
 
       // const $h5 = $('<h5>').text(villager.personality)
       // .appendTo($div)
 
-      const $image =
-        $('<img>')
-        .addClass('photo')
-        .attr('src', (villager.image_uri))
-        .appendTo($div)
-      }
-  })
-})
+      const $image = $("<img>")
+        .addClass("photo")
+        .attr("src", villager.image_uri)
+        .appendTo($div);
+    }
+  });
+});
