8c065c5c6258a318997c4866fe552c9ac6c8dce2
Mimi Netto <michnetto249@gmail.com>
2020-09-02 10:27:52 -0400

    wednesday morning commit

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ $(() => {
     type: "GET",
     dataType: "json",
   }).then((villagers) => {
-    const sliceArr = villagers.slice(0, 20);
-    console.log(sliceArr);
+    const sliceArr = villagers.slice(0, 50);
+    // console.log(sliceArr);
     for (let villager of sliceArr) {
       const $villagers = $(".villagers");
 
@@ -16,8 +16,9 @@ $(() => {
 
       const $h3 = $("<h3>").text(villager.name["name-USen"]).appendTo($div);
 
-      // const $h5 = $('<h5>').text(villager.personality)
-      // .appendTo($div)
+      const $h5 = $('<h5>').text(villager.personality)
+      .addClass('personality')
+      .appendTo($div)
 
       const $image = $("<img>")
         .addClass("photo")
